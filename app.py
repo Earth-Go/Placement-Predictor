93a1b9933f510bf0b006a7310c35d9e567c2ea14
Earth-Go <24ucs097@lnmiit.ac.in>
2025-06-07 14:55:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.write("Enter your academic details below to predict your placement chance.")
 
 # Input fields
 cgpa = st.slider("CGPA", 4.00, 10.00, 7.00, 0.01)
-iq = st.slider("IQ", 60, 140, 110)
+iq = st.slider("IQ", 60, 150, 110)
 profile_score = st.slider("Profile Score", 30.0, 100.0, 50.0, 0.5)
 
 if st.button("Predict Placement"):
